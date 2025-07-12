53e14ef46c15ccf2b860f188710bd8f5e9863287
Aditya <adityapanigrahy98@gmail.com>
2025-07-12 17:36:58 +0530

    inital setup for frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className="items-center justify-center flex h-screen bg-black">
+      <p className="text-white text-3xl text-center">Yoo Frontend has started..</p>
+    </div>
+  )
+}
+
+export default App
